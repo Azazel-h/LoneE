f68db3bb686dfe94363b35d69c228ed1103b7fdd
Azazel_h <arseniy2003bulgakov@gmail.com>
2021-09-29 11:04:14 +0300

    Better architecture

--- LonE-core/Renderer.cpp
+++ LonE-core/Renderer.cpp
@@ -6,24 +6,69 @@
 #include "Renderer.h"
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vector>
 
-namespace lone {
-    renderer::Window::Window(int width, int height, const char *name) {
+namespace lone::renderer {
+    Window::Window(int width, int height, const char *name) {
         this->width = width;
         this->height = height;
         this->name = name;
-        Init();
+        initVulcan();
+        cleanUp();
     }
 
-    void renderer::Window::Init() const {
+    void Window::createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = this->name;
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+
+        vkEnumerateInstanceExtensionProperties(nullptr, &glfwExtensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(glfwExtensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &glfwExtensionCount, extensions.data());
+
+        std::cout << "available extensions:\n";
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+
+        if (vkCreateInstance(&createInfo, nullptr, &this->instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    };
+
+    void Window::initVulcan() {
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         if (glfwInit()) {
-            GLFWwindow *window = glfwCreateWindow(this->width, this->height, this->name, nullptr, nullptr);
+            this->window = glfwCreateWindow(this->width, this->height, this->name, nullptr, nullptr);
+            createInstance();
             while (!glfwWindowShouldClose(window)) {
                 glfwPollEvents();
+                VkSurfaceKHR surface;
+                VkResult err = glfwCreateWindowSurface(this->instance, window, nullptr, &surface);
+
             }
-            glfwDestroyWindow(window);
-            glfwTerminate();
         }
+    }
+
+    void Window::cleanUp() {
+        vkDestroyInstance(this->instance, nullptr);
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
 }
--- LonE-core/Renderer.h
+++ LonE-core/Renderer.h
@@ -1,13 +1,21 @@
-namespace lone {
-    namespace renderer {
+#include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+namespace lone::renderer {
         class Window {
         private:
             int width, height;
             const char *name;
+            VkInstance instance;
+            GLFWwindow *window;
         public:
             Window(int width, int height, const char *name);
-            void Init() const;
+            void initVulcan();
+            void cleanUp();
+        protected:
+            void createInstance();
         };
     }
-}
 
